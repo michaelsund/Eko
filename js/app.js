881ccd15f02b1ef96dec735bd8ceaf5e5a2807f6
Michael Sund <michael@osund.com>
2015-10-03 19:23:54 +0200

    moving to desktop

--- js/app.js
+++ js/app.js
@@ -1,109 +1,230 @@
-var app = angular.module('StarterApp', ['ngMaterial', 'googlechart', 'ngMdIcons']);
+var app = angular.module('StarterApp', ['ngMaterial', 'googlechart', 'ngMdIcons', 'angularMoment']);
+
+app.run(function(amMoment) {
+  amMoment.changeLocale('sv');
+});
 
 app.controller('AppCtrl', ['$scope', '$http', '$mdSidenav', function($scope, $http, $mdSidenav) {
   console.log('loaded main controller');
-  // Alaways load the main layout file, application start point
-  $scope.nav = 'partials/main.html';
-  // Mock data, for last 3 months
-  $scope.data = [
+
+  // Vars
+  $scope.selected = {
+    'month': new moment().format('MMMM'),
+    'year': new moment().format('YYYY')
+  };
+
+  $scope.datasetSalary = [];
+  $scope.datasetCosts = [];
+  $scope.monthsAvailable = [];
+  $scope.yearsAvailable = [];
+  $scope.plusminus = 0;
+  $scope.plusminusSign = '';
+  $scope.newCost = {
+    'cost': null,
+    'category': null
+  };
+  $scope.newMoney = {
+    'amount': null,
+    'desc': null
+  };
+
+
+  // Mock data
+  $scope.categories = [
+    'Räkningar', 'Övrigt', 'Bilkostnader', 'Kläder'
+  ];
+
+  $scope.salary = [
     {
-      month: 'Janurai',
-      amount: 100,
-      category: 'övrigt'
+      'year': 2015,
+      'months': [
+        {
+          'month':'Oktober',
+          'data': [
+            {type:'Skatt',value:2000,by:'Michael'},
+            {type:'Lön',value:10000,by:'Emelie'}
+          ]
+        }
+      ]
     },
     {
-      month: 'Februari',
-      amount: 200,
-      category: 'mat'
+      'year': 2014,
+      'months': [
+        {
+          'month':'Januari',
+          'data': [
+            {type:'Skatt',value:1000,by:'Michael'},
+            {type:'Lön',value:3000,by:'Emelie'}
+          ]
+        }
+      ]
+    }
+  ];
+  $scope.costs = [
+    {
+      'year': 2015,
+      'months': [
+        {
+          'month':'Oktober',
+          'data': [
+            {
+              'category':'Räkningar',
+              'amount':7000,
+              'date':'2015-10-01',
+              'by':'Michael'
+            }
+          ]
+        },
+        {
+          'month':'September',
+          'data': [
+            {
+              'category':'Räkningar',
+              'amount':2000,
+              'date':'2015-09-01',
+              'by':'Michael'
+            }
+          ]
+        }
+      ]
     },
     {
-      month: 'Februari',
-      amount: 100,
-      category: 'mat'
-    },
-    {
-      month: 'Mars',
-      amount: 400,
-      category: 'räkningar'
-    },
-    {
-      month: 'Mars',
-      amount: 300,
-      category: 'räkningar'
+      'year': 2014,
+      'months': [
+        {
+          'month':'Januari',
+          'data': [
+            {
+              'category':'Räkningar',
+              'amount':1000,
+              'date':'2015-10-01',
+              'by':'Michael'
+            }
+          ]
+        },
+        {
+          'month':'November',
+          'data': [
+            {
+              'category':'Räkningar',
+              'amount':20000,
+              'date':'2015-09-01',
+              'by':'Michael'
+            }
+          ]
+        }
+      ]
     }
   ];
 
-  $scope.initialFetch = function() {
-    $scope.totalThisMonth = 0;
-    for (x in $scope.data) {
-      // will change, set current month before check in loop
-      if ($scope.data[x].month === 'Mars') {
-        $scope.totalThisMonth += $scope.data[x].amount;
-        console.log('adding ' + $scope.data[x].amount);
-      }
-    }
+  $scope.saveCost = function() {
+    // console.log('saving ' + $scope.newCost.cost + ' at ' + $scope.newCost.category);
   };
-  $scope.initialFetch();
+
+  $scope.saveNewMoney = function() {
+    console.log('saving ' + $scope.newMoney.amount + ' at ' + $scope.newMoney.desc);
+    $scope.newMoney.amount = null;
+    $scope.newMoney.desc = null;
+  };
+
+  $scope.clearCost = function() {
+    // $scope.newCost.cost = null;
+    // $scope.newCost.category = null;
+  };
+
+  $scope.loadCategories = function() {
+  };
+
+  // Alaways load the main layout file, application start point
+  $scope.nav = 'partials/main.html';
+
   // Menu item selection and loading of partials to content area
   $scope.doNav = function(partial) {
     switch (partial) {
       case null:
         $scope.nav = 'partials/main.html';
+        $mdSidenav('left').toggle();
         break;
       case 'overview':
         $scope.nav = 'partials/main.html';
+        $mdSidenav('left').toggle();
         break;
-<<<<<<< HEAD
-      case 'add':
-        $scope.nav = 'partials/add.html';
+      case 'addmoney':
+        $scope.nav = 'partials/addmoney.html';
+        $mdSidenav('left').toggle();
+        break;
+      case 'addcost':
+        $scope.nav = 'partials/addcost.html';
+        $mdSidenav('left').toggle();
         break;
       case 'graph':
         $scope.nav = 'partials/graph.html';
+        $mdSidenav('left').toggle();
         break;
       case 'details':
         $scope.nav = 'partials/details.html';
-=======
-      case 'write':
-        $scope.nav = 'partials/new.html';
->>>>>>> 1b80cf5ff332e78f87814ad10a53963570651ed0
+        $mdSidenav('left').toggle();
+        break;
+      case 'details-view':
+        $scope.nav = 'partials/details.html';
         break;
       default:
         $scope.nav = 'partials/main.html';
+        $mdSidenav('left').toggle();
+        break;
     }
 
-    // Close the sidenav when a selection is made, only affects small devices
-    $mdSidenav('left').toggle();
   };
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
 
+  $scope.dummy = function() {};
+
+  $scope.$watch('selected.year', function() {
+    console.log('year changed ' + $scope.selected.year);
+  });
+
+
+  $scope.loadMonths = function() {
+    $scope.monthsAvailable = [];
+    for (x in $scope.datasetCosts) {
+      $scope.monthsAvailable.push($scope.datasetCosts[x].month);
+      console.log('TO AVAILABLE ' + $scope.datasetCosts[x].month);
+    }
+  };
+
+
+  // Calculations
+  $scope.calculations = function() {
+    console.log($scope.selected.year);
+    $scope.yearsAvailable = [];
+    $scope.datasetSalary = [];
+    $scope.datasetCosts = [];
+    console.log('running calculations');
+    for (x in $scope.salary) {
+      $scope.yearsAvailable.push($scope.salary[x].year);
+      // Use the current year as dataset, filter month later when picked in dropdown
+      if ($scope.salary[x].year == $scope.selected.year) {
+        $scope.datasetSalary = $scope.salary[x].months;
+      }
+    }
+    for (x in $scope.costs) {
+      // Use the current year as dataset, filter month later when picked in dropdown
+      if ($scope.costs[x].year == $scope.selected.year) {
+        $scope.datasetCosts = $scope.costs[x].months;
+      }
+    }
+  };
+
   $scope.fetchData = function() {
     console.log('fetching data');
-    // Mock data
-    $scope.salarysThisMonth = [
-      20000,
-      10000
-    ];
+    $scope.totalCostSelectedMonth = 0;
+    $scope.totalSalarySelectedMonth = 0;
 
-    $scope.thisMonth = [
-      {
-        'category':'Räkningar',
-        'amount':7000,
-        'date':'2015-01-01'
-      },
-      {
-        'category':'Mat',
-        'amount':2000,
-        'date':'2015-01-02'
-      }
-    ];
-
-    // Calculations
-    $scope.totalSalaryThisMonth = 0;
-    for (x in $scope.salarysThisMonth) {
-      $scope.totalSalaryThisMonth += $scope.salarysThisMonth[x];
-    }
+    $scope.calculations(function() {
+      $scope.loadMonths();
+    });
   };
   $scope.fetchData();
 }]);
